652711581d3275cd0a68eee4a8febecea363aba7
adcerro <79855505+adcerro@users.noreply.github.com>
2024-08-30 19:50:44 -0500

    punto 1 taller 3

---
+++ taller-03.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {String} letra Una letra.
+ * @returns {Boolean} True si es vocal.
+ */
+function esVocal(letra){
+    return /(a|e|i|o|u)/i.test(letra);
+}
+/**
+ * Punto 1.
+ * @param {String} string El texto.
+ * @param {String} voc vocales o consonantes.
+ * @returns 
+ */
+function desglosarString(string,voc="vocales"){
+    if(string.length===0){
+        return 0;
+    }
+    if(voc==="vocales"){
+        return string.split("").filter(esVocal).length;
+    }
+    
+    if(voc==="consonantes"){
+        return string.split("").filter((letra)=>!esVocal(letra)).length;
+    }
+}
