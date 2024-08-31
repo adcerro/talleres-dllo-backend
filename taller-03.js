4bff1794d72dd698e74bb7ff2148b1e9100b8eb2
adcerro <79855505+adcerro@users.noreply.github.com>
2024-08-30 19:58:21 -0500

    más documentación

--- taller-03.js
+++ taller-03.js
@@ -10,7 +10,7 @@ function esVocal(letra){
  * Punto 1.
  * @param {String} string El texto.
  * @param {String} voc vocales o consonantes.
- * @returns 
+ * @returns {Number} El numero de vocales o consonantes en el string.
  */
 function desglosarString(string,voc="vocales"){
     if(string.length===0){
@@ -19,7 +19,6 @@ function desglosarString(string,voc="vocales"){
     if(voc==="vocales"){
         return string.split("").filter(esVocal).length;
     }
-    
     if(voc==="consonantes"){
         return string.split("").filter((letra)=>!esVocal(letra)).length;
     }
