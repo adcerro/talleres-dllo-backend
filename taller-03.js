489e831a73b36d772949c76d45b523a4a431de0b
adcerro <79855505+adcerro@users.noreply.github.com>
2024-09-01 14:28:46 -0500

    conversion unitaria romano a decimal

--- taller-03.js
+++ taller-03.js
@@ -40,6 +40,26 @@ function twoSum(lista,numero){
 function esNumeroRomano(texto){
     return !/[^IVXLCDM]/.test(texto);
 }
+function romanoAdecimal(numero){
+    switch (numero) {
+        case "I":
+            return 1;
+        case "V":
+            return 5;
+        case "X":
+            return 10;
+        case "L":
+            return 50;
+        case "C":
+            return 100;
+        case "D":
+            return 500;
+        case "M":
+            return 1000;
+        default:
+            return 0;
+    }
+}
 function conversionRomana(numero){
     if(!esNumeroRomano(numero)){
         throw new Error("No es número romano");
